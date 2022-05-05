966bd963e09f9beee92fb560ce95c2d3b67a1d83
PabloBluAgent <pablo@bluagent.com>
2022-05-04 17:03:31 -0700

    Pagina principal y cuestionario, dropdown y arreglo de menu

--- js/app.js
+++ js/app.js
@@ -1,7 +1,39 @@
-function cambiarClase(){
-    let siteNav = document.getElementById('site-nav');
-        siteNav.classList.toggle('site-nav-open');
-    let menuOpen = document.getElementById('menu-toggle');
-        menuOpen.classList.toggle('menu-open');    
-        
+function cambiarClase() {
+  let siteNav = document.getElementById("site-nav");
+  siteNav.classList.toggle("site-nav-open");
+  let menuOpen = document.getElementById("menu-toggle");
+  menuOpen.classList.toggle("menu-open");
 }
+
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides((slideIndex += n));
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides((slideIndex = n));
+// }
+
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " active";
+// }
